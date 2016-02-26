448bbe9b09418cc25005f4a161f45231cdf9aaf4
Guilherme Sa <guilsa001@gmail.com>
2016-02-26 14:23:47 -0800

    Add boleto transaction endpoint

--- controllers/index.rb
+++ controllers/index.rb
@@ -1,16 +1,43 @@
-# require 'pry'
+require 'pry'
 
 get '/' do
   erb :index
 end
 
 get '/sucesso' do
+  @status = params["status"]
   erb :success
 end
 
 get '/erro' do
   @status = params["status"]
   erb :unsuccess
+end
+
+post '/boletotransactions/new' do
+
+  BOLETO_AMOUNT = params["amount"]
+
+  PagarMe.api_key = ENV['PAGARME_SECRET']
+
+  transaction = PagarMe::Transaction.new({
+      :amount => BOLETO_AMOUNT,
+      :payment_method => "boleto"
+  })
+
+  transaction.charge
+
+  boleto_url = transaction.boleto_url # URL do boleto bancário
+  boleto_barcode = transaction.boleto_barcode # código de barras do boleto bancário
+
+  status = transaction.status
+
+  if status == 'waiting_payment' then
+    redirect to("/sucesso?status=#{status}")
+  else
+    redirect to("/erro?status=#{status}")
+  end
+
 end
 
 post '/transactions/new' do
